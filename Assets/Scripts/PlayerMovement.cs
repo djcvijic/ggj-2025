1d54f306554c3cd1619832712227dd03ed43c9db
aleksat <aleksat@twodesperados.com>
2025-01-24 23:07:07 +0100

    Added Player logic and Game Settings

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement
+{
+    private Player _player;
+    private InertialMovement _inertialMovement;
+    private Transform _transform;
+
+    private Vector3 Position
+    {
+        get => _transform.position;
+        set => _transform.position = value;
+    }
+
+    public PlayerMovement(Player player)
+    {
+        _player = player;
+        _transform = _player.transform;
+        _inertialMovement = new InertialMovement();
+    }
+
+    public void UpdatePosition()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        Vector3 newPosition = _inertialMovement.CalculateNewPosition(Position, input, Time.deltaTime);
+
+        Position = newPosition;
+    }
+}
