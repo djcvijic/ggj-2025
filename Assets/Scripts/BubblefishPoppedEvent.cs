b90e49494454e2bb4327129ebce1d95e55095072
aleksat <aleksat@twodesperados.com>
2025-01-25 14:57:15 +0100

    Created bubblefish popped event sctipt

---
+++ Assets/Scripts/BubblefishPoppedEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class BubblefishPoppedEvent
+{
+    private event Action<Bubblefish> _bubblefishPopped;
+
+    public void AddEvent(Action<Bubblefish> toAdd)
+    {
+        _bubblefishPopped += toAdd;
+    }
+
+    public void RemoveEvent(Action<Bubblefish> toRemove)
+    {
+        _bubblefishPopped -= toRemove;
+    }
+
+    public void OnBubblefishPopped(Bubblefish bubblefish)
+    {
+        _bubblefishPopped?.Invoke(bubblefish);
+    }
+}
