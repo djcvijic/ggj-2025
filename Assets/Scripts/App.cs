8bebe9f37d6687a5ab18601f2b7b4b5c6766b2a8
aleksat <aleksat@twodesperados.com>
2025-01-26 00:01:40 +0100

    Game Win Condition Added. Empty boss script

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -4,13 +4,13 @@ public class App : MonoSingleton<App>
 {
     public GameSettings GameSettings;
     public EventsNotifier EventsNotifier;
+    public GameWin GameWin;
 
     [SerializeField] private Player playerPrefab;
     [field: SerializeField] public BubblefishManager BubblefishManager { get; private set; }
     [field: SerializeField] public EnemyManager EnemyManager { get; private set; }
     [SerializeField] private AppCanvas appCanvas;
 
-    private CameraFollow _cameraFollow;
     private Player player;
 
     protected override void Awake()
@@ -18,12 +18,15 @@ public class App : MonoSingleton<App>
         base.Awake();
 
         EventsNotifier = new EventsNotifier();
-        
+        GameWin = new GameWin();
+
         appCanvas.Initialize();
         
         player = Instantiate(playerPrefab, GameSettings.PlayerSpawnY * Vector3.up, Quaternion.identity);
-        _cameraFollow = CameraFollow.InstantiateCameraFollowObj().Initialize(player.transform, player.GetComponent<Rigidbody2D>());
+        
+        CameraFollow.InstantiateCameraFollowObj().Initialize(player.transform, player.GetComponent<Rigidbody2D>());
         BubblefishManager.Initialize(player);
         EnemyManager.Initialize(() => player.transform.position);
+        
     }
 }
