fb6bc0bd8bf6638a40ffb3b080f994cc08b9a48e
aleksat <aleksat@twodesperados.com>
2025-01-26 02:40:59 +0100

    Added bubblefish progress bar update

--- Assets/Scripts/PlayerStatusBars.cs
+++ Assets/Scripts/PlayerStatusBars.cs
@@ -1,20 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatusBars : MonoBehaviour
 {
     [SerializeField] private TMP_Text bubblefishNumberText;
-
+    
+    [SerializeField] private Image bubblefishProgressBarFill;
+    [SerializeField] private List<ProgressBarFishIndicator> progressBarFishIndicators;
+    [SerializeField] private RectTransform bubblefishProgressBarHolder;
+    
+    [SerializeField] private DepthIndicator depthIndicator;
+    [SerializeField] private RectTransform depthHolder;
+    
     private int TotalBubblefishesPopped => App.Instance.BubblefishManager.BubblefishPopped;
 
     public void Initialize()
     {
         App.Instance.EventsNotifier.BubblefishPopped += OnBubblefishPopped;
+        
+        SetUpFishIndicatorPositions();
     }
 
     private void OnBubblefishPopped(Bubblefish bubblefish)
     {
         bubblefishNumberText.text = TotalBubblefishesPopped.ToString();
+    }
+
+    private void Update()
+    {
+        UpdateSideBarPointer();
+        UpdateTopBarProgress();
+    }
+
+
+    private void UpdateTopBarProgress()
+    {
+        bubblefishProgressBarFill.fillAmount = App.Instance.BubblefishManager.BubblefishPoppedPercentage / 100f;
+    }
+
+    private void UpdateSideBarPointer()
+    {
+        
+    }
+
+    private void SetUpFishIndicatorPositions()
+    {
+        
     }
 }
