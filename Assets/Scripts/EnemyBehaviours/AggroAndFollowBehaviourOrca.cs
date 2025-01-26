410b839bae0810ced83bd257b30615736e54d2ee
aleksat <aleksat@twodesperados.com>
2025-01-26 16:51:15 +0100

    Orca rotation

---
+++ Assets/Scripts/EnemyBehaviours/AggroAndFollowBehaviourOrca.cs
@@ -0,0 +1,43 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AggroAndFollowBehaviourOrca : EnemyBehaviour
+{
+    public override void Initialize(Enemy enemy)
+    {
+        var spriteRotator = enemy.AddComponent<SpriteRotator>();
+        spriteRotator.Initialize(() => -GetMoveDirection());
+    }
+
+    public override void Execute(float deltaTime)
+    {
+        if (Enemy.IsDead)
+            return;
+
+        var t = Enemy.transform;
+        var position = t.position;
+        var diff = PlayerPositionGetter() - (Vector2)position;
+        if (!ShouldFollowPlayer(diff))
+            return;
+
+        var direction = diff.normalized;
+        Enemy.Rigidbody.MovePosition(position + deltaTime * Enemy.Info.MoveSpeed * (Vector3)direction);
+    }
+
+    private Vector2 GetMoveDirection()
+    {
+        var t = Enemy.transform;
+        var position = t.position;
+        var diff = PlayerPositionGetter() - (Vector2)position;
+
+        var direction = diff.normalized;
+        return direction;
+
+    }
+
+    private bool ShouldFollowPlayer(Vector2 diff)
+    {
+        return diff.magnitude < Enemy.Info.AggroRange;
+    }
+}
